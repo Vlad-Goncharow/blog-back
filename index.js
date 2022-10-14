2a655ea8fab3e06b016252d1f8508f815adcb816
vladyslav <goncharow159@gmail.com>
2022-10-14 09:33:17 +0300

    test access

--- index.js
+++ index.js
@@ -16,7 +16,15 @@ mongoose.connect(process.env.MONGODB_URL)
 const app = express()
 
 app.use(express.json())
+
 app.use(cookieParser())
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.use(cors({
   credentials: true,
   origin: process.env.CLIENT_URL
